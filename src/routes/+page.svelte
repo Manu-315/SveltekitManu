60fe3ad866d8d9d7b5ddd8ef097e52c78694f299
Manu-315 <harlander.manuel@johak.at>
2024-02-27 13:51:25 +0100

    supabase hinzufügen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<div class="prose">also try the options in <code>/src/routes/+layout.js</code>!</div>
+<script>
+    import {supabase} from '$lib/supabase';
+    
+    let promise = supabase.from("countries").select();
+</script>
+
+{#await promise}
+    <p>Loading...</p>
+{:then result}
+  <div>{JSON.stringify(result)}</div>
+
+  {/await}
