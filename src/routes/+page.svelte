31f041b813f8d2cc5bca7cf26c29e7f66ffa66c6
Manu-315 <harlander.manuel@johak.at>
2024-02-27 14:05:46 +0100

    aufgaben geadded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,23 @@
     let promise = supabase.from("countries").select();
 </script>
 
+<div class ="prose">
+<h1>Aufgaben</h1>
+
+<ul><li>anstatt eines JS objects dass auf der seite ist, möchte ich eine liste aller länder</li>
+
+<li>
+    Bei Daisy UI ein loading symbol einbinden
+
+</li>
+
+<li>
+    gib die vercel URL zum Projekt ab
+
+</li>
+</ul>
+</div>
+
 {#await promise}
     <p>Loading...</p>
 {:then result}
